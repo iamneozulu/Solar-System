2f17d46e22cccf51b68d1d89c1530e54cbeac339
WeThinkCode_ <wethinkcode_@pop-os.localdomain>
2024-04-30 07:30:11 +0200

    Rings added

--- celestialObjects.js
+++ celestialObjects.js
@@ -2,58 +2,56 @@ import * as THREE from 'three';
 
 export class Star extends THREE.Mesh{
     constructor(size, rotationSpeed, texture){
-        const starTexture = new THREE.SphereGeometry(size, 32, 32);
-        starTexture.generateMipmaps = true;
-        starTexture.minFilter = THREE.LinearMipmapLinearFilter;
-
-        const starMesh = new THREE.MeshBasicMaterial(
+        const starMesh = new THREE.SphereGeometry(size, 32, 32);
+        
+        const starTexture = new THREE.MeshBasicMaterial(
             {map: new THREE.TextureLoader().load(texture)})
 
-        super( starTexture, starMesh );
+        starTexture.generateMipmaps = true;
+        starTexture.minFilter = THREE.LinearMipmapLinearFilter;
+            
+        super( starMesh, starTexture );
 
         this.size = size;
         this.rotationSpeed = rotationSpeed;
         this.texture = texture;
         this.rotation.y += 1
     };
-
-    onClick() {
-        this.texture = "public_assets/textures/sun.jpg";
-    };
 };
 
 export class Planet extends THREE.Mesh{
-    constructor(size, planetOrbit, distancePerYear, rotationSpeed, texture){
-        const planetTexture = new THREE.SphereGeometry(size, 16, 16);
+    constructor(size, planetOrbit, distancePerYear, rotationSpeed, axisTilt, texture){
+
+        const planetMesh = new THREE.SphereGeometry(size, 16, 16);
+
+        const planetTexture = new THREE.MeshBasicMaterial(
+            {map: new THREE.TextureLoader().load(texture)})
+
         planetTexture.generateMipmaps = true;
         planetTexture.minFilter = THREE.LinearMipmapLinearFilter;
 
-        const planetMesh = new THREE.MeshBasicMaterial(
-            {map: new THREE.TextureLoader().load(texture)})
-
-        super( planetTexture, planetMesh );
+        super( planetMesh, planetTexture );
 
         this.size = size;
         this.orbitRadius = planetOrbit * 20;
         this.orbitSpeed = distancePerYear / 600000000000;
         this.rotationSpeed = rotationSpeed;
+        this.axisTilt = axisTilt
         this.texture = texture;
-    };
-    onClick() {
-        this.texture = "public_assets/textures/sun.jpg";
     };
 };
 
 export class Moon extends THREE.Mesh{
     constructor(size, orbitRadius, orbitSpeed, rotationSpeed, texture){
-        const moonTexture = new THREE.SphereGeometry(size, 8, 8);
-        moonTexture.generateMipmaps = true;
-        moonTexture.minFilter = THREE.LinearMipmapLinearFilter;
-
-        const moonMesh = new THREE.MeshBasicMaterial(
+        const moonMesh = new THREE.SphereGeometry(size, 8, 8);
+        
+        const moonTexture = new THREE.MeshBasicMaterial(
             {map: new THREE.TextureLoader().load(texture)})
 
-        super( moonTexture, moonMesh );
+        moonTexture.generateMipmaps = true;
+        moonTexture.minFilter = THREE.LinearMipmapLinearFilter;
+            
+        super( moonMesh, moonTexture );
 
         this.size = size;
         this.orbitRadius = orbitRadius;
@@ -61,25 +59,52 @@ export class Moon extends THREE.Mesh{
         this.rotationSpeed = rotationSpeed;
         this.texture = texture;
     };
-    onClick() {
-        this.texture = "public_assets/textures/sun.jpg";
+};
+
+export class PlanetRing extends THREE.Mesh{
+    constructor(planet, innerCircumference, outCircumference){
+        const geometry = new THREE.RingGeometry( planet.size + innerCircumference, planet.size + outCircumference, 24); 
+        const material = new THREE.MeshBasicMaterial( { color: 'lightgray', side: THREE.DoubleSide } );
+
+        super( geometry, material )
     };
 };
 
-export function starRotation(star){
-    star.rotation.y += 0.001
+export class OrbitRing extends THREE.Mesh{
+    constructor(planet){
+        const geometry = new THREE.RingGeometry( planet.orbitRadius-1, planet.orbitRadius+1, 62); 
+        const material = new THREE.MeshBasicMaterial( { color: 'lightgray', side: THREE.DoubleSide } );
+
+        super( geometry, material )
+    }
 }
 
+export function starRotation(star){
+    star.rotation.y += 0.001
+};
+
 export function planetOrbit(planet){
+    
     planet.position.x = planet.orbitRadius * Math.cos(planet.orbitSpeed * Date.now());
     planet.position.z = planet.orbitRadius * Math.sin(planet.orbitSpeed * Date.now());
+    planet.rotation.x = THREE.MathUtils.degToRad(planet.axisTilt)
     planet.rotation.y += planet.rotationSpeed
 };
 
 export function moonOrbit(moon, planet){
-    planet.position.x = planet.orbitRadius * Math.cos(planet.orbitSpeed * Date.now());
-    planet.position.z = planet.orbitRadius * Math.sin(planet.orbitSpeed * Date.now());
+    // planet.position.x = planet.orbitRadius * Math.cos(planet.orbitSpeed * Date.now());
+    // planet.position.z = planet.orbitRadius * Math.sin(planet.orbitSpeed * Date.now());
 
     moon.position.x = planet.position.x + moon.orbitRadius * Math.cos(moon.orbitSpeed * Date.now());
     moon.position.z = planet.position.z + moon.orbitRadius * Math.sin(moon.orbitSpeed * Date.now());
-}
+};
+
+export function planetRingOrbit(ring, planet){
+    ring.position.x = planet.position.x
+    ring.position.z = planet.position.z
+    ring.rotation.x = THREE.MathUtils.degToRad(planet.axisTilt + 270)
+};
+
+export function orbitRing(orbitRing){
+    orbitRing.rotation.x = THREE.MathUtils.degToRad(270)
+};
--- main.js
+++ main.js
@@ -12,16 +12,34 @@ let INTERSECTED;
 const pointer = new THREE.Vector2();
 
 //Initializing planets
-const Sun = new CELESTIAL.Star(10, 3, "public_assets/textures/sun.jpg" )
-const Mercury = new CELESTIAL.Planet(1, 1, (392000000/1.205), 3, "public_assets/textures/mercury.jpg")
-const Venus = new CELESTIAL.Planet(1.9, 2, (684000000/3.075), 3, "public_assets/textures/venus.jpg")
-const Earth = new CELESTIAL.Planet(2, 3, (942000000/5), 3, "public_assets/textures/earth.jpg")
-const Lunar = new CELESTIAL.Moon(1, 4, (2420000/136.61), 3, "public_assets/textures/moon.jpg")
-const Mars = new CELESTIAL.Planet(1.5, 4, (1440000000/9.4), 3, "public_assets/textures/mars.jpg")
-const Jupiter = new CELESTIAL.Planet(5, 8, (4770000000/60), 3, "public_assets/textures/jupiter.jpg")
-const Saturn = new CELESTIAL.Planet(4.5, 10, (9120000000/147), 3, "public_assets/textures/saturn.jpg")
-const Uranus = new CELESTIAL.Planet(4, 12, (18400000000/420), 3, "public_assets/textures/uranus.jpg")
-const Neptune = new CELESTIAL.Planet(4, 15, (28100000000/825), 3, "public_assets/textures/neptune.jpg")
+const Sun = new CELESTIAL.Star(10, 3, "public_assets/textures/sun.jpg" );
+
+const Mercury = new CELESTIAL.Planet(1, 1, (392000000/1.205), 3, 0.027, "public_assets/textures/mercury.jpg");
+const MercuryOrbit = new CELESTIAL.OrbitRing(Mercury);
+
+const Venus = new CELESTIAL.Planet(1.9, 2, (684000000/3.075), 0.05, 177.36, "public_assets/textures/venus.jpg");
+const VenusOrbit = new CELESTIAL.OrbitRing(Venus);
+
+const Earth = new CELESTIAL.Planet(2, 3, (942000000/5), 0.05, -23.5,"public_assets/textures/earth.jpg");
+const Lunar = new CELESTIAL.Moon(1, 4, (2420000/136.61), 0.05, "public_assets/textures/moon.jpg");
+const EarthOrbit = new CELESTIAL.OrbitRing(Earth);
+
+const Mars = new CELESTIAL.Planet(1.5, 4, (1440000000/9.4), 0.05, 25, "public_assets/textures/mars.jpg");
+const MarsOrbit = new CELESTIAL.OrbitRing(Mars);
+
+const Jupiter = new CELESTIAL.Planet(5, 8, (4770000000/60), 3, 3.13, "public_assets/textures/jupiter.jpg");
+const JupiterOrbit = new CELESTIAL.OrbitRing(Jupiter);
+
+const Saturn = new CELESTIAL.Planet(4.5, 10, (9120000000/147), 3, 26.73, "public_assets/textures/saturn.jpg");
+const SaturnRing = new CELESTIAL.PlanetRing(Saturn, 1, 5);
+const SaturnOrbit = new CELESTIAL.OrbitRing(Saturn);
+
+const Uranus = new CELESTIAL.Planet(4, 12, (18400000000/420), 3, 97.77, "public_assets/textures/uranus.jpg");
+const UranusRing = new CELESTIAL.PlanetRing(Uranus, 3, 4)
+const UranusOrbit = new CELESTIAL.OrbitRing(Uranus);
+
+const Neptune = new CELESTIAL.Planet(4, 15, (28100000000/825), 3, 28,"public_assets/textures/neptune.jpg");
+const NeptuneOrbit = new CELESTIAL.OrbitRing(Neptune);
 
 
 init()
@@ -29,7 +47,7 @@ animate()
 
 //Initializes all important features like camera, renderer, controls etc
 function init() {
-
+    
     //renderer
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio( window.devicePixelRatio )
@@ -39,7 +57,7 @@ function init() {
     
     // scene
     scene = new THREE.Scene();
-
+    
     //space HDRI
     const HDRIloader = new RGBELoader();
     HDRIloader.load('public_assets/textures/HDR_hazy_nebulae.hdr', function(texture){
@@ -47,23 +65,41 @@ function init() {
         scene.background = texture;
         scene.environment = texture;
     });
-
+    
     scene.add(Sun);
+    
     scene.add(Mercury);
+    scene.add(MercuryOrbit);
+    
     scene.add(Venus);
+    scene.add(VenusOrbit);
+    
     scene.add(Earth);
     scene.add(Lunar);
+    scene.add( EarthOrbit );
+    
     scene.add(Mars);
+    scene.add(MarsOrbit);
+    
     scene.add(Jupiter);
+    scene.add(JupiterOrbit);
+    
     scene.add(Saturn);
+    scene.add(SaturnRing);
+    scene.add(SaturnOrbit);
+    
     scene.add(Uranus);
+    scene.add(UranusRing);
+    scene.add(UranusOrbit);
+    
     scene.add(Neptune);
+    scene.add(NeptuneOrbit);
     
     
     //camera
     camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 50000 );
-    camera.position.x = 500;
-    camera.position.y = 100;
+    camera.position.x = 1000;
+    camera.position.y = 700;
     
     // orbit controls
     controls = new OrbitControls( camera, renderer.domElement );
@@ -72,7 +108,8 @@ function init() {
         MIDDLE: THREE.MOUSE.DOLLY
     };
     controls.minDistance = 100
-    controls.maxDistance = 1000
+    controls.maxDistance = 1500
+    controls.maxPolarAngle = THREE.MathUtils.degToRad(90)
     controls.panSpeed = 1
 
     raycaster = new THREE.Raycaster();
@@ -133,17 +170,36 @@ function animate() {
 //Handles calculating planet orbits
 function render() {
 
-    //Orbit and rotation calculations
+    // Orbit and rotation calculations
     CELESTIAL.starRotation(Sun);
+
     CELESTIAL.planetOrbit(Mercury);
+    CELESTIAL.orbitRing(MercuryOrbit);
+    
     CELESTIAL.planetOrbit(Venus);
+    CELESTIAL.orbitRing(VenusOrbit);
+    
     CELESTIAL.planetOrbit(Earth);
     CELESTIAL.moonOrbit(Lunar, Earth);
+    CELESTIAL.orbitRing(EarthOrbit);
+
     CELESTIAL.planetOrbit(Mars);
+    CELESTIAL.orbitRing(MarsOrbit);
+
     CELESTIAL.planetOrbit(Jupiter);
+    CELESTIAL.orbitRing(JupiterOrbit);
+
     CELESTIAL.planetOrbit(Saturn);
+    CELESTIAL.planetRingOrbit(SaturnRing ,Saturn);
+    CELESTIAL.orbitRing(SaturnOrbit);
+
     CELESTIAL.planetOrbit(Uranus);
+    CELESTIAL.planetRingOrbit(UranusRing ,Uranus);
+    CELESTIAL.orbitRing(UranusOrbit);
+
     CELESTIAL.planetOrbit(Neptune);
+    CELESTIAL.orbitRing(NeptuneOrbit);
+
 
     controls.update();
     renderer.render( scene, camera );
